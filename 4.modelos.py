43a33343d064ebb1bb851290757a2e8e80448bb1
santiagoalmeida08 <santiago.almeida@udea.edu.co>
2024-04-26 11:06:32 -0500

    hacer redes_componenetes_verificar variables

--- 4.modelos.py
+++ 4.modelos.py
@@ -14,7 +14,7 @@ from sklearn.model_selection import cross_val_score
 from sklearn.pipeline import make_pipeline
 import funciones as fn
 from sklearn.feature_selection import SelectFromModel
-
+from sklearn import metrics
 
 # conexión a la base de datos
 
@@ -32,6 +32,15 @@ x = df.drop(['readmitted'], axis=1)
 y = df['readmitted']
 y_mod = y.replace({'yes':1, 'no':0})
 
+y_mod.value_counts()
+
+
+df.info()
+
+x=x.drop(['diag_1','diag_2','diag_3','change'], axis=1)
+x=x[['time_in_hospital', 'n_lab_procedures', 'n_procedures', 'n_medications']]
+
+df['change'].value_counts()
 # Encoding variables #
 
 df1=x.copy()
@@ -39,8 +48,8 @@ df1=x.copy()
 df1.dtypes
 
 #Dummies
-list_dummies =['medical_specialty','diag_1','diag_2','diag_3','glucose_test','A1Ctest']
-
+#list_dummies =['medical_specialty','diag_1','diag_2','diag_3','glucose_test','A1Ctest']
+list_dummies =['medical_specialty','glucose_test','A1Ctest']
 #Ordinal - edad
 list_ordinal = ['edad']  
 
@@ -53,9 +62,10 @@ x_encoded = fn.encode_data(df1, list_label, list_dummies,list_ordinal)
 #Escalado de variables 
 
 scaler = StandardScaler()
-Xesc = scaler.fit_transform(x_encoded) 
-Xesc1 = pd.DataFrame(Xesc, columns = x_encoded.columns)
+Xesc = scaler.fit_transform(x) 
+Xesc1 = pd.DataFrame(Xesc, columns = x.columns)
 
+Xesc1.info()
 #Seleccion e importancia de variables 
 
 modelo_importance = LogisticRegression().fit(Xesc1,y_mod)
@@ -72,11 +82,26 @@ var_select = fn.sel_variables(LogisticRegression(),Xesc1,y_mod,threshold="2*mean
 
 # Desempeño de los modelos 
 
-mod_tree = DecisionTreeClassifier()
-mod_rand = RandomForestClassifier()
+mod_rand= DecisionTreeClassifier(min_samples_leaf=2)
+mod_rand = RandomForestClassifier(min_samples_leaf=2)
 #mod_xgbosting = XGBClassifier()
 mod_log = LogisticRegression()
 modelos = list([mod_tree,mod_rand,mod_log])
+
+
+mod_rand.fit(Xesc1, y_mod)
+mod_rand.feature_importances_
+mod_rand.feature_names_in_
+
+y_pred=mod_rand.predict(Xesc1)
+
+y_mod2=np.array(y_mod)
+
+metrics.accuracy_score(y_mod2,y_pred)
+
+scores=cross_val_score(mod_rand,Xesc1,y_mod, scoring='accuracy', cv=10)
+pdscores=pd.DataFrame(scores)
+
 
 
 def medir_modelos(modelos,scoring,X,y,cv):
@@ -92,7 +117,7 @@ def medir_modelos(modelos,scoring,X,y,cv):
     metric_modelos.columns=["decision_tree","random_forest","reg_logistic"]
     return metric_modelos   
 
-mod = medir_modelos(modelos,"accuracy",Xesc,y_mod,10)
+mod = medir_modelos(modelos,"accuracy",Xesc,y_mod,3)
 
 
 f1s= mod
